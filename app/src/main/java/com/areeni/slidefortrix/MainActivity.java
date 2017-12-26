2af6151352efefba4e0911acc089cb12685e78ef
omrysn <omrysn4@gmail.com>
2017-12-26 12:13:03 -0700

    added icons credits

--- app/src/main/java/com/areeni/slidefortrix/MainActivity.java
+++ app/src/main/java/com/areeni/slidefortrix/MainActivity.java
@@ -7,15 +7,15 @@ import android.content.res.Resources;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 import java.util.Locale;
@@ -68,23 +68,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.language) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage(R.string.chooseLanguage)
-                    .setCancelable(false)
-                    .setPositiveButton(R.string.english, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            setLocale("en");
-                        }
-                    })
-                    .setNegativeButton(R.string.arabic, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            setLocale("ar");
-                        }
-                    });
-            AlertDialog alert = builder.create();
-            alert.show();
+            Toast.makeText(this, "Working on adding Arabic Support", Toast.LENGTH_SHORT).show();
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setMessage(R.string.chooseLanguage)
+//                    .setCancelable(false)
+//                    .setPositiveButton(R.string.english, new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int id) {
+//                            setLocale("en");
+//                        }
+//                    })
+//                    .setNegativeButton(R.string.arabic, new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int id) {
+//                            setLocale("ar");
+//                        }
+//                    });
+//            AlertDialog alert = builder.create();
+//            alert.show();
             return true;
         }else if(id == R.id.about){
+            showAbout();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -100,6 +102,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent refresh = new Intent(this, MainActivity.class);
         startActivity(refresh);
         finish();
+    }
+
+
+    private void showAbout() {
+        // Inflate the about message contents
+        View messageView = getLayoutInflater().inflate(R.layout.about_layout, null, false);
+
+        // When linking text, force to always use default color. This works
+        // around a pressed color state bug.
+        TextView textView1 =  messageView.findViewById(R.id.kingIcon);
+        TextView textView2 =  messageView.findViewById(R.id.othericons);
+        textView1.setMovementMethod(LinkMovementMethod.getInstance());
+        textView2.setMovementMethod(LinkMovementMethod.getInstance());
+//        int defaultColor = textView.getTextColors().getDefaultColor();
+//        textView.setTextColor(defaultColor);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setIcon(R.drawable.iclan);
+        builder.setTitle(R.string.app_name);
+        builder.setView(messageView);
+        builder.create();
+        builder.show();
     }
 }
 //todo: Add singles mode
